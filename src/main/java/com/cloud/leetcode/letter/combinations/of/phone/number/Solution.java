0ed0cf86a5bcd0be8c2c155dd1a55788b8a78668
cloud <fliuheyan@gmail.com>
2020-09-02 18:48:31 -0700

    sep 1st.

--- src/main/java/com/cloud/leetcode/letter/combinations/of/phone/number/Solution.java
+++ src/main/java/com/cloud/leetcode/letter/combinations/of/phone/number/Solution.java
@@ -1,15 +1,35 @@
 package com.cloud.leetcode.letter.combinations.of.phone.number;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Solution {
 
     public List<String> letterCombinations(String digits) {
-        List<String> list = new ArrayList<>();
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < digits.length(); i++) {
+            int value = Character.getNumericValue(digits.charAt(i));
+            if (value == 1) {
+                continue;
+            }
+            if (value == 9) {
+                temp.add(str.substring(24));
+            } else {
+                temp.add(str.substring((value - 1) * 3, (value - 1) * 3 + 2));
+            }
+        }
+        for(int i=0;i<temp.size();i++) {
+            
+        }
         return list;
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        String str = "23";
+
+        System.out.println(new BigDecimal("0.000").compareTo(BigDecimal.ZERO) == 0);
     }
 }
