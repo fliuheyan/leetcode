83a57956ab823481e36b2d649088e321cf4e8267
fliuheyan <fliuheyan@gmail.com>
2020-12-22 00:44:03 +0800

    need for offer 04

---
+++ src/main/java/string/replace/space/Solution.java
@@ -0,0 +1,17 @@
+package string.replace.space;
+
+import com.google.common.base.Preconditions;
+
+public class Solution {
+    public String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String input = "We are happy.";
+        String result = solution.replaceSpace(input);
+
+        Preconditions.checkArgument(result.equals("We%20are%20happy."));
+    }
+}
